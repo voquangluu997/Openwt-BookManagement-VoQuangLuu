c78798474bf5f1d7daca954366fcdea0f8dfae96
voquangluu997 <voquangluu997@gmail.com>
2021-07-20 16:02:56 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { TransformInterceptor } from './tranform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   console.log(process.env.PORT);
