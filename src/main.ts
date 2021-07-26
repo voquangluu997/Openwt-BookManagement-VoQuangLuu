37b1e5d419f61bb408a6d64f8de388b1985726d9
voquangluu997 <voquangluu997@gmail.com>
2021-07-27 01:16:25 +0700

    enable guard for books

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './tranform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
