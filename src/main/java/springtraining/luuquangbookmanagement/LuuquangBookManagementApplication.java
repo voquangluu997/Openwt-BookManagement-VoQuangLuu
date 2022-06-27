ec8e648aaf863383fac839c5285819437579112a
liuliu <voquangluu997@gmail.com>
2022-06-27 14:34:17 +0700

    init project

---
+++ src/main/java/springtraining/luuquangbookmanagement/LuuquangBookManagementApplication.java
@@ -0,0 +1,13 @@
+package springtraining.luuquangbookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuuquangBookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuuquangBookManagementApplication.class, args);
+	}
+
+}
